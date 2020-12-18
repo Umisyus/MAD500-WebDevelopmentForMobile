3e26a0b5636adabcfbf6f9313e5c3bc1d8a968f2
Umisyus <umit007_@hotmail.com>
2020-12-18 00:08:44 -0500

    Renamed App from AngularStoreApp to UmitsStoreInventoryApp

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('AngularStoreApp app is running!');
+    expect(page.getTitleText()).toEqual('UmitsStoreApp app is running!');
   });
 
   afterEach(async () => {
