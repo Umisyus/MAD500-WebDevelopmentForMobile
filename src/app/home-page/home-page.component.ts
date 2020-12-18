6b8dc2b81fcc1c2783a73c1432ea2da086085b69
Umisyus <umit007_@hotmail.com>
2020-12-17 19:23:20 -0500

    Added delete, Works.

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {DataProviderService} from '../Services/data-provider.service';
 
 @Component({
   selector: 'app-home-page',
@@ -8,7 +9,26 @@ import {Component, OnInit} from '@angular/core';
 
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dps: DataProviderService) {
+  }
+
+  items = [];
+
+  genItems(): void {
+    for (let i = 0; i < 3; i++) {
+      this.items.push(
+        {
+          id: i,
+          title: `Title ${i}`,
+          description: `Descripiton ${i}`,
+          dateAdded: `Added on: 12/${i}/2020`,
+          imageURL: `http://1.1.1.1/${i}.jpg`,
+        },
+      );
+      // Add to array
+      this.dps.addItem(this.items[i]).subscribe(value => console.log(`Added: ${value.title}`));
+    }
+  }
 
   ngOnInit(): void {
   }
